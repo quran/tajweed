3b6eb4b51080dd84356ff8c945581a4f72ea95a1
asimmohiuddin <asimmohiuddin@users.noreply.github.com>
2016-01-03 23:57:23 -0500

    Update TajweedRules.java

--- TajweedRules.java
+++ TajweedRules.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class TajweedRules{
   public static void main (String args[]){
-    String verse = "الْحَمْدُ لِلَّهِ رَبِّ الْعَالَمِينَ";//sample verse
-    int lenght = verse.length();
+    String arabicText = "الْحَمْدُ لِلَّهِ رَبِّ الْعَالَمِينَ";//sample verse
+    int lenght = arabicText.length();
     
     //general set up
     for(int i = 0; i < length; i++){
