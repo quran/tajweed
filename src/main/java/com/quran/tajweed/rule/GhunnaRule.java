04a6dd5532ebb23d77031f82028dadad9c209bc9
asimmohiuddin <asimmohiuddin@users.noreply.github.com>
2016-01-05 13:14:10 -0500

    use ints and Characters instead of char

--- src/main/java/com/quran/tajweed/rule/GhunnaRule.java
+++ src/main/java/com/quran/tajweed/rule/GhunnaRule.java
@@ -12,32 +12,32 @@ public class GhunnaRule implements Rule {
     System.out.println("Checking ghunna...");
     int length = ayah.length();
     int startPos = 0, endPos = 0;
+    int currentChar = ayah.codePointAt(i);
     for (int i = 0; i < length; i++){
-      char[] previous = {'!', '!', '!', '!', '!'};
-      char[] next = {'!', '!', '!', '!', '!'};
+      int[] previous = {0, 0, 0, 0, 0};
+      int[] next = {0, 0, 0, 0, 0};
       for (int j = 0; j < previous.length; j++){
         int nIndex = i + j + 1;
         int pIndex = i - j - 1;
         if(nIndex < length){
-          next[j] = ayah.charAt(nIndex);
+          next[j] = ayah.codePointAt(nIndex);
         }
         if(pIndex >= 0){
-          previous[j] = ayah.charAt(pIndex);
+          previous[j] = ayah.codePointAt(pIndex);
         }
       }
-      char currentChar = ayah.charAt(i);
+      
       if ((currentChar == CharacterUtil.NOON ||
         currentChar == CharacterUtil.MEEM) &&
-        next[0] == CharacterUtil.SHADDA){
-
+        next[0] == CharacterUtil.SHADDA){ //next[0] can be changed later to make it more robust
           startPos = i - findPreviousLetterPronounced(previous);
           endPos = i + findNextLetter(next);
-          System.out.println("match from: " + startPos + " till " + endPos + ", letter: " + currentChar + " next[]="+next[0]);
+          System.out.println("match from: " + startPos + " till " + endPos + ", letter: " + Character.toChars(currentChar));
       }
     }
   }
 
-  private int findPreviousLetterPronounced (char[] previous){
+  private int findPreviousLetterPronounced (int[] previous){
     for (int i = 1; i < previous.length; i++){
       if(CharacterUtil.isLetter(previous[i]) && CharacterUtil.isDiaMark(previous[i-1])){
         return i;
@@ -46,7 +46,7 @@ public class GhunnaRule implements Rule {
     return 0;
   }
 
-  private int findNextLetter (char[] next) {
+  private int findNextLetter (int[] next) {
     for (int i = 0; i < next.length; i++){
       if(!CharacterUtil.isDiaMark(next[i])){
         return i;
