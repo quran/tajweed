46380cc260944b38a098a50d3b04a10dedc98917
ahmedre <ahmedre@gmail.com>
2016-01-04 23:46:11 -0800

    Fix iqlab rule to also check tanween.

--- src/main/java/com/quran/tajweed/rule/IqlabRule.java
+++ src/main/java/com/quran/tajweed/rule/IqlabRule.java
@@ -4,31 +4,28 @@ import com.quran.tajweed.CharacterUtil;
 
 /**
  * Iqlab Rule
- * Iqlab occurs when a ن (noon sakina) is followed by a ب.
+ * Iqlab occurs when a ن (noon sakina) or tanween is followed by a ب.
  */
 public class IqlabRule implements Rule {
 
   @Override
   public void checkAyah(String ayah) {
     System.out.println("checking iqlab...");
-    int ayahLength = ayah.length();
 
     int index = -1;
-    while ((index = (ayah.indexOf(CharacterUtil.NOON, index + 1))) > -1) {
-      int nextIndex = index + Character.charCount(ayah.codePointAt(index));
-      if (nextIndex < ayahLength) {
-        int next = ayah.codePointAt(nextIndex);
-        if (Character.isSpaceChar(next)) {
-          if (++nextIndex < ayahLength) {
-            next = ayah.codePointAt(nextIndex);
-          } else {
-            break;
-          }
-        }
+    while ((index = (ayah.indexOf(CharacterUtil.BA, index + 1))) > -1) {
+      int previous = ayah.codePointBefore(index);
 
-        if (next == CharacterUtil.BA) {
-          System.out.println("match at " + index);
-        }
+      int lastIndex = index;
+      while (Character.isSpaceChar(previous) && lastIndex > 0) {
+        previous = ayah.codePointBefore(lastIndex--);
+      }
+
+      if (previous == CharacterUtil.FATHA_TANWEEN ||
+          previous == CharacterUtil.DAMMA_TANWEEN ||
+          previous == CharacterUtil.KASRA_TANWEEN ||
+          previous == CharacterUtil.NOON) {
+        System.out.println("match at " + index);
       }
     }
   }
