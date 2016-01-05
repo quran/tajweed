0f56d978631f72d9d680f11b18ce5917f7072bbd
asimmohiuddin <asimmohiuddin@users.noreply.github.com>
2016-01-05 14:16:53 -0500

    Reworked Qalqalah
    Cleaned up the code so it isn't totally terrible

--- src/main/java/com/quran/tajweed/rule/QalqalahRule.java
+++ src/main/java/com/quran/tajweed/rule/QalqalahRule.java
@@ -10,61 +10,59 @@ import com.quran.tajweed.CharacterUtil;
 
 public class QalqalahRule implements Rule {
 
+  private static final Character BA = CharacterUtil.BA;
+  private static final Character JEEM = 0x062c;
+  private static final Character DAAL = 0x062f;
+  private static final Character QAAF = 0x0642;
+  private static final Character TAA = 0x0637;
+
   @Override
   public void checkAyah(String ayah) {
+    System.out.println("checking qalqalah...");
     int length = ayah.length();
+    int startPos = 0, endPos = 0;
     for (int i = 0; i < length; i++) {
-      char c = ayah.charAt(i);
-      char c1 = '!', c2 = '!', c3 = '!', cp1 = '!', cp2 = '!', cp3 = '!';
-      boolean shaddaAtEnd = false;
-      if (i + 1 < length) {
-        c1 = ayah.charAt(i + 1);
-      }
-      if (i + 2 < length) {
-        c2 = ayah.charAt(i + 2);
-      }
-      if (i + 3 < length) {
-        c3 = ayah.charAt(i + 3);
-      }
-      if (i - 1 >= 0) {
-        cp1 = ayah.charAt(i - 1);
-      }
-      if (i - 2 >= 0) {
-        cp2 = ayah.charAt(i - 2);
-      }
-      if (i - 3 >= 0) {
-        cp3 = ayah.charAt(i - 3);
+      int[] next = {0, 0, 0, 0, 0};
+      int currentChar = ayah.codePointAt(i);
+      for (int j = 0; j < next.length; j++) {
+        int nIndex = i + j + 1;
+        if (nIndex < length) {
+          next[j] = ayah.codePointAt(nIndex);
+        }
       }
 
-      //qalqalah
-      if (c1 == 'ّ' && (i + 3 >= length || CharacterUtil.isEndMark(c3))) {
-        shaddaAtEnd = true;
+      if ((currentChar == DAAL ||
+          currentChar == BA ||
+          currentChar == JEEM ||
+          currentChar == QAAF ||
+          currentChar == TAA) &&
+          (next[0] == CharacterUtil.SUKUN || weStopping(next)))    {
+        startPos = i;
+        endPos = i + remaingMarks(next);
+        System.out.println("match from: " + startPos + " till " + endPos + ", letter: " + Character.toChars(currentChar));
       }
-      //checks wheter ending letter has shadda since this will cause an extra character to be taken into account
-      int tillQalqalah = i
-          + 2; //normally Qalqalah has the letter itself and a sukoon/case ending during a stop
-      if (shaddaAtEnd) {
-        tillQalqalah++; //in this case (like surah Masad verse 1) there is additional one char length
-      }
-      if (c2 == '!') {
-        tillQalqalah = length; //if the ayah is ending, it should go till end of ayah then
-      }
-      if ((c == 'د') || (c == 'ب') || (c == 'ج') || (c == 'ق') || (c
-          == 'ط')) { //is this even qalqalah? lol
-        if (c2 == '!' || c1 == 'ْ' || CharacterUtil.isEndMark(c2)
-            || shaddaAtEnd) {//c1==sukoon. btw if statement separated for simplicity
-            /*
-            arabicText.setSpan(new ForegroundColorSpan(Color.RED),
-                i, tillQalqalah, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            the above was used in Android app to go ahead and set the color
-            but now we will use it to set position. At this point in the code,
-            we know that there is qalqalah at the position i in the verse. We also
-            know by using tillQalqlah, how many chars to include in the font coloring.
-            The position i and end position tillQalqalah can be stored in a db file for
-            this specific verse.
-            */
-        }
-      } //end qalqalah check
     }
+  }
+
+  private boolean weStopping(int[] next){
+    for (int i = 0; i < next.length; i++){
+      if (CharacterUtil.isEndMark(next[i]) || next[i] == 0) {
+        return true;
+      }
+      if(CharacterUtil.isLetter(next[i])){
+        break;
+      }
+    }
+    return false;
+  }
+
+  private int remaingMarks(int[] next){
+    int i = 0;
+    for (; i < next.length; i++){
+      if (!CharacterUtil.isDiaMark(next[i])) {
+        break;
+      }
+    }
+    return i;
   }
 }
