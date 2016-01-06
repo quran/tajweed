3faa9469813b240ad009066ecde0c3e6b5f0d44d
ahmedre <ahmedre@gmail.com>
2016-01-06 00:23:26 -0800

    Improve performance of idgham rule.
    Previously, we were going through the string 6 times (once for each
    potential idgham letter), and looking for matches of that letter in the
    string. This change makes the algorithm walk the string only once.

--- src/main/java/com/quran/tajweed/rule/IdghamRule.java
+++ src/main/java/com/quran/tajweed/rule/IdghamRule.java
@@ -11,55 +11,51 @@ import com.quran.tajweed.util.CharacterInfo;
  * Igham without ghunna: when ن or tanween are followed by ل or ر
  */
 public class IdghamRule implements Rule {
+  // with ghunna
   private static final Character YA = 0x064a;
-  private static final Character RA = 0x0631;
   private static final Character MEEM = CharacterUtil.MEEM;
-  private static final Character LAM = 0x0644;
   private static final Character WAW = 0x0648;
   private static final Character NOON = CharacterUtil.NOON;
 
-  private static final Character[] IDGHAM_WITH_GUNNAH = new Character[] { YA, NOON, MEEM, WAW };
-  private static final Character[] IDGHAM_WITHOUT_GUNNAH = new Character[] { LAM, RA };
+  // without ghunna
+  private static final Character RA = 0x0631;
+  private static final Character LAM = 0x0644;
 
   @Override
   public void checkAyah(String ayah) {
-    checkIdghamWithGhunna(ayah);
-    checkIdghamWithoutGhunna(ayah);
-  }
+    System.out.println("checking idgham...");
 
-  private void checkIdghamWithGhunna(String ayah) {
-    System.out.println("checking idgham with ghunnah...");
-    checkIdghamHelper(ayah, IDGHAM_WITH_GUNNAH);
-  }
+    int[] characters = ayah.codePoints().toArray();
+    for (int i = 0; i < characters.length; i++) {
+      int current = characters[i];
 
-  private void checkIdghamWithoutGhunna(String ayah) {
-    System.out.println("checking idgham without ghunnah...");
-    checkIdghamHelper(ayah, IDGHAM_WITHOUT_GUNNAH);
-  }
-
-  private void checkIdghamHelper(String ayah, Character[] prefixCharacters) {
-    for (Character potentialPostfix : prefixCharacters) {
-      int index = -1;
-      while ((index = (ayah.indexOf(potentialPostfix, index + 1))) > -1) {
-        CharacterInfo previousCharacter = CharacterUtil.getPreviousCharacter(ayah, index);
-
-        int previous = previousCharacter.character;
-        if (previous == CharacterUtil.FATHA_TANWEEN ||
-            previous == CharacterUtil.DAMMA_TANWEEN ||
-            previous == CharacterUtil.KASRA_TANWEEN ||
-            previous == NOON) {
-          System.out.println("match at: " + index + ", letter: " + potentialPostfix);
-        } else if (previousCharacter.index > 0 &&
-            (previous == CharacterUtil.ALEF_LAYINA ||
-             previous == CharacterUtil.ALEF)) {
-          // if the previous character is either alif layina or alif, check to see if we have
-          // tanween with fatha on the previous letter.
-          previous = ayah.codePointBefore(previousCharacter.index);
-          if (previous == CharacterUtil.FATHA_TANWEEN) {
-            System.out.println("match at: " + index + ", letter: " + potentialPostfix);
-          }
+      if (current == RA || current == LAM ||
+          current == YA || current == NOON || current == MEEM || current == WAW) {
+        if (isValidIdgham(ayah, i)) {
+          String type = (current != LAM && current != RA) ? "with ghunna" : "without ghunna";
+          System.out.println("match idgham " + type + " at: " + i + ", letter: " + (char) current);
         }
       }
     }
+  }
+
+  private boolean isValidIdgham(String ayah, int index) {
+    CharacterInfo previousCharacter = CharacterUtil.getPreviousCharacter(ayah, index);
+
+    boolean result = false;
+    int previous = previousCharacter.character;
+    if (previous == CharacterUtil.FATHA_TANWEEN ||
+        previous == CharacterUtil.DAMMA_TANWEEN ||
+        previous == CharacterUtil.KASRA_TANWEEN ||
+        previous == NOON) {
+      result = true;
+    } else if (previousCharacter.index > 0 &&
+        (previous == CharacterUtil.ALEF_LAYINA || previous == CharacterUtil.ALEF)) {
+      // if the previous character is either alif layina or alif, check to see if we have
+      // tanween with fatha on the previous letter.
+      previous = ayah.codePointBefore(previousCharacter.index);
+      result = previous == CharacterUtil.FATHA_TANWEEN;
+    }
+    return result;
   }
 }
