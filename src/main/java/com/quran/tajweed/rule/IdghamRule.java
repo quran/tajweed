816a44521f0c9a70904ae045d2bb0a54424da573
Asim Mohiuddin <a0mohi01@a0mohi01.T-mobile.com>
2018-10-07 08:51:29 -0400

    Naskh mode support for Idgham Rule
    1. created methods out of common code between Naskh and Madani
    2. Added a check to remove cases of Idgham applying to within words (made a space necessary between noon sakin/tanween and yarmaloon letters to avoid recognizing "Idgham" in words like 'dunya')

--- src/main/java/com/quran/tajweed/rule/IdghamRule.java
+++ src/main/java/com/quran/tajweed/rule/IdghamRule.java
@@ -17,6 +17,10 @@ import java.util.List;
  * Igham without ghunna: when ن or tanween are followed by ل or ر
  */
 public class IdghamRule implements Rule {
+  private final boolean isNaskhMode;
+  public IdghamRule(boolean isNaskhMode) {
+    this.isNaskhMode = isNaskhMode;
+  }
   // with ghunna
   private static final Character YA = 0x064a;
   private static final Character MEEM = CharacterUtil.MEEM;
@@ -33,12 +37,32 @@ public class IdghamRule implements Rule {
     int[] characters = ayah.codePoints().toArray();
     for (int i = 0; i < characters.length; i++) {
       int current = characters[i];
-
-      if (current == RA || current == LAM ||
-          current == YA || current == NOON || current == MEEM || current == WAW) {
+      boolean isYarmaloon = isYarmaloon(current);
+      int[] previous = CharacterUtil.getPreviousChars(ayah, i);
+      int[] next = CharacterUtil.getNextChars(ayah, i);
+      if (isNaskhMode){
+        int endPos = i + CharacterUtil.findNextLetterPronounced(next);
+        int[] next2 = CharacterUtil.getNextChars(ayah, endPos);
+        int nextPronounced = CharacterUtil.findNextLetterPronounced(next);
+        //If noon sakin or tanween is followed by a letter of Yarmaloon
+        if ((CharacterUtil.isTanween(next[0]) || CharacterUtil.isNoonSaakin(next)) &&
+                isGhunnaYarmaloon(next[nextPronounced])){
+          //Make sure it is not the important exception to this rule - when idgham criteria is met WITHIN a word
+          boolean trueIdgham = false;
+          for (int nextIndex = 1; nextIndex < nextPronounced; nextIndex++){
+            if (next[nextIndex] == ' ') trueIdgham = true;
+          }
+          if (trueIdgham){
+            results.add(new Result(ResultType.IDGHAM_WITH_GHUNNA,
+                    i - CharacterUtil.findPreviousLetterPronounced(previous),
+                    endPos + CharacterUtil.findRemainingMarks(next2)));
+          }
+        }
+      }
+      if (isYarmaloon && !isNaskhMode) {
         int previousMatchPosition = isValidIdgham(ayah, i);
         if (previousMatchPosition >= 0) {
-          boolean withGhunna = (current != LAM && current != RA);
+          boolean withGhunna = (isGhunnaYarmaloon(current));
           if (withGhunna) {
             results.add(new TwoPartResult(ResultType.IDGHAM_WITH_GHUNNA, i, i + 1,
                 ResultType.IDGHAM_NOT_PRONOUNCED, previousMatchPosition,
@@ -73,5 +97,14 @@ public class IdghamRule implements Rule {
       previousIndex = previousCharacter.index - 1;
     }
     return result ? previousIndex : -1;
+  }
+
+  private boolean isYarmaloon(int thisChar){
+    return (thisChar == RA || thisChar == LAM ||
+            thisChar == YA || thisChar == NOON || thisChar == MEEM || thisChar == WAW);
+  }
+
+  private boolean isGhunnaYarmaloon(int thisChar){
+    return (thisChar == YA || thisChar == NOON || thisChar == MEEM || thisChar == WAW);
   }
 }
